6e7948b3623bf72a9dcd8ca7d9a9745ad7386bc3
Koray Aksoy <burakkorayaksoy@hotmail.com>
2022-04-18 02:06:36 +0300

    Models, controller and config files created

---
+++ src/main/java/com/korayaks/chatapp/model/Status.java
@@ -0,0 +1,7 @@
+package com.korayaks.chatapp.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
---
+++ src/main/java/com/korayaks/chatapp/model/User.java
@@ -0,0 +1,20 @@
+package com.korayaks.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@Table(name="Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+}
